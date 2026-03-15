cf955571a9cd79ee380d371002e61bafbba79fb5
mac <samecatalina@gmail.com>
2026-03-15 18:54:33 +0200

    Update CMS config and remove unused auth files

--- api/auth.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const client_id = process.env.GITHUB_CLIENT_ID;
-  const redirect_uri = `https://tum-web-lab2-chi.vercel.app/api/callback`;
-  const scope = "repo,user";
-
-  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`;
-
-  res.redirect(githubAuthUrl);
-}
